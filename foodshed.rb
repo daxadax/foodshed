bdcf0e358c2874028b0ed6796487f8be4fc235a8
dax <d.dax@email.com>
2015-11-08 21:09:18 +0100

    Add display page helper

--- foodshed.rb
+++ foodshed.rb
@@ -1,9 +1,14 @@
 class Foodshed < Sinatra::Application
   get '/' do
+    display_page 'all'
   end
 end
 
 helpers do
+  def display_page(title, locals: {}, layout: 'default')
+    haml title.to_sym, layout: "layouts/#{layout}".to_sym, locals: locals
+  end
+
   def link_to(url,text=url,opts={})
     attributes = ""
     opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
